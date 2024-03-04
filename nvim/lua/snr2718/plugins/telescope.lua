eec6043c56d7613d558c881e67d535eb853b64fb
Sathya Ragunathan <53249506+snr2718@users.noreply.github.com>
2024-03-04 18:10:42 +0530

    telescope-live-grep-args

--- nvim/lua/snr2718/plugins/telescope.lua
+++ nvim/lua/snr2718/plugins/telescope.lua
@@ -1,13 +1,24 @@
 return {
   'nvim-telescope/telescope.nvim', 
   branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    { 
+      "nvim-telescope/telescope-live-grep-args.nvim" ,
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    }
+  },
   config = function()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    --vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    require("telescope").load_extension("live_grep_args")
+    vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
   end
 }
 
