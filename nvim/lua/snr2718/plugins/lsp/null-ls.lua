1bb4fcef14e27f9a299ce7786a1f1e38255f71a4
snr2718 <sathya.naruto@gmail.com>
2024-07-14 22:40:50 +0530

    rename cspell to codespell in null-ls

--- nvim/lua/snr2718/plugins/lsp/null-ls.lua
+++ nvim/lua/snr2718/plugins/lsp/null-ls.lua
@@ -7,7 +7,7 @@ return {
     local null_ls = require('null-ls')
     null_ls.setup({
       sources = {
-        null_ls.builtins.diagnostics.cspell.with({
+        null_ls.builtins.diagnostics.codespell.with({
            diagnostics_postprocess = function(diagnostic)
              diagnostic.severity = vim.diagnostic.severity.HINT
            end
