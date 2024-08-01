4799647605f2a04f8f9cc9cef3abaf44bab18110
Sathya Ragunathan <53249506+snr2718@users.noreply.github.com>
2024-08-01 11:26:26 +0530

    added hardtime.lua

--- nvim/lua/snr2718/plugins/colorscheme.lua
+++ nvim/lua/snr2718/plugins/colorscheme.lua
@@ -2,9 +2,18 @@ return {
   {
     'sainnhe/gruvbox-material',
     name='gruvbox-material',
+    enabled = true,
     config = function()
       vim.cmd('colorscheme gruvbox-material')
       vim.cmd("highlight MatchParen guibg=red ctermbg=red")
     end,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    enabled = false,
+    config = function()
+      vim.cmd('colorscheme kanagawa')
+    end
   }
 }
