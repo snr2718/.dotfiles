ca9164077eb0c2da7a168e9f2f5d1ed3dde50b07
snr2718 <sathya.naruto@gmail.com>
2024-12-06 04:30:27 +0000

    Added a Failed state for markdown todo list

--- nvim/lua/snr2718/plugins/render-markdown.lua
+++ nvim/lua/snr2718/plugins/render-markdown.lua
@@ -1,7 +1,21 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+	---@module 'render-markdown'
+	---@type render.md.UserConfig
+	opts = {},
+	config = function()
+		require("render-markdown").setup({
+			checkbox = {
+				custom = {
+					important = {
+						raw = "[~]",
+						rendered = "⚠ ",
+						highlight = "DiagnosticError",
+						scope_highlight = "@markup.strikethrough",
+					},
+				},
+			},
+		})
+	end,
 }
