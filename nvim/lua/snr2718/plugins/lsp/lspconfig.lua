dcaca8dff69d07fba0249f74c0d771eb330d652e
snr2718 <sathya.naruto@gmail.com>
2024-12-17 08:07:10 +0000

    added code action keymap

--- nvim/lua/snr2718/plugins/lsp/lspconfig.lua
+++ nvim/lua/snr2718/plugins/lsp/lspconfig.lua
@@ -42,6 +42,7 @@ return {
 				vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
 				vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
 				vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+				vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 			end,
 		})
 
