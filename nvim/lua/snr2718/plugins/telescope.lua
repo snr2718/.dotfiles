f908f9b3344ae47eca0cfad1314715555965cee7
snr2718 <sathya.naruto@gmail.com>
2024-12-17 10:41:11 +0000

    add telescope keymap to see the list of diagnostics for a buffer

--- nvim/lua/snr2718/plugins/telescope.lua
+++ nvim/lua/snr2718/plugins/telescope.lua
@@ -12,15 +12,20 @@ return {
 	},
 	config = function()
 		local builtin = require("telescope.builtin")
+		require("telescope").load_extension("live_grep_args")
+
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-		require("telescope").load_extension("live_grep_args")
 		vim.keymap.set("n", "<leader>fg", function()
 			local word = vim.fn.expand("<cword>") -- Get the word under the cursor
 			require("telescope.builtin").live_grep({ default_text = word })
 		end, { noremap = true, silent = true, desc = "Live grep word under cursor" })
+
+		vim.keymap.set("n", "<leader>fd", function()
+			require("telescope.builtin").diagnostics({ bufnr = 0 })
+		end, { noremap = true, silent = true, desc = "List Buffer Diagnostics" })
 	end,
 }
