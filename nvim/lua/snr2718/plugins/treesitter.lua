aa691264f148d56bd34438524e6129cad4e2ca74
snr2718 <sathya.naruto@gmail.com>
2025-02-03 09:20:45 +0000

    add python to treesitter's ensure_installed

--- nvim/lua/snr2718/plugins/treesitter.lua
+++ nvim/lua/snr2718/plugins/treesitter.lua
@@ -1,13 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build=":TSUpdate", 
-  config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {"c", "lua", "vim", "vimdoc", "html", "javascript", "typescript", "scss", "svelte", "go"},
-      sync_install = false,
-      highlight = {enable=true},
-      indent = {enable=true}
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"html",
+				"javascript",
+				"typescript",
+				"scss",
+				"svelte",
+				"go",
+				"python",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
