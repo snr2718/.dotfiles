351451b3e4fccb1da2311e5a38cbbb27b6b11b9d
snr2718 <sathya.naruto@gmail.com>
2025-02-03 09:19:01 +0000

    use fG for livegrep of highlighted word

--- nvim/lua/snr2718/plugins/telescope.lua
+++ nvim/lua/snr2718/plugins/telescope.lua
@@ -15,11 +15,13 @@ return {
 		require("telescope").load_extension("live_grep_args")
 
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		--vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<Leader>fr", builtin.oldfiles, {})
+
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
-		vim.keymap.set({ "n", "v" }, "<leader>fg", function()
+		vim.keymap.set({ "n", "v" }, "<leader>fG", function()
 			local word
 
 			-- Check if in Visual mode
