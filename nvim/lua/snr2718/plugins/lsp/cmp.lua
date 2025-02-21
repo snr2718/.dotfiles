b0e08e6c049d4d257393623e24f9c953c6bcf770
snr2718 <sathya.naruto@gmail.com>
2025-02-21 01:12:06 +0000

    disabled <C-y> for nvim-cmp

--- nvim/lua/snr2718/plugins/lsp/cmp.lua
+++ nvim/lua/snr2718/plugins/lsp/cmp.lua
@@ -1,70 +1,70 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    -- Autocompletion
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'saadparwaiz1/cmp_luasnip' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-nvim-lua' },
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-nvim-lua" },
 
-    -- Snippets
-    { 'L3MON4D3/LuaSnip' },
+		-- Snippets
+		{ "L3MON4D3/LuaSnip" },
+	},
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require('luasnip');
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-y>"] = cmp.config.disable, -- Disabled, to use it with copilot, remove line to enable
+				["<C-n"] = cmp.mapping.select_next_item(),
+				["<C-p"] = cmp.mapping.select_prev_item(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+				{ name = "path" },
+				{ name = "luasnip" },
+			},
+			completion = { completeopt = "menu,menuone,noinsert" },
+			preselect = cmp.PreselectMode.None,
+		})
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n"] = cmp.mapping.select_next_item(),
-        ["<C-p"] = cmp.mapping.select_prev_item(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" })
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-        { name = "luasnip" },
-      },
-      completion = { completeopt = "menu,menuone,noinsert" },
-      preselect = cmp.PreselectMode.None
-    })
+		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-    vim.diagnostic.config({
-      virtual_text = {
-        source = true,
-      },
-      float = {
-        source = true,
-      }
-    })
-  end
+		vim.diagnostic.config({
+			virtual_text = {
+				source = true,
+			},
+			float = {
+				source = true,
+			},
+		})
+	end,
 }
