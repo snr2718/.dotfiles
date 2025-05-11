91116ddf94997935e7902e98dddccf5380dcc891
snr2718 <sathya.naruto@gmail.com>
2025-05-11 09:33:57 +0000

    updated colorscheme

--- nvim/lua/snr2718/plugins/colorscheme.lua
+++ nvim/lua/snr2718/plugins/colorscheme.lua
@@ -1,50 +1,7 @@
-local function select_colorscheme(scheme)
-	local colorschemes = {
-		gruvbox = {
-			"morhetz/gruvbox",
-			name = "gruvbox",
-			config = function()
-				vim.cmd("colorscheme gruvbox")
-				vim.cmd("highlight MatchParen guibg=red ctermbg=red")
-			end,
-		},
-		kanagawa = {
-			"rebelot/kanagawa.nvim",
-			name = "kanagawa",
-			config = function()
-				require("kanagawa").setup({
-					overrides = function()
-						return {
-							Visual = { bg = "#3B5B6E" },
-						}
-					end,
-				})
-				vim.cmd("colorscheme kanagawa")
-			end,
-		},
-		tokyonight = {
-			"folke/tokyonight.nvim",
-			name = "tokyonight",
-			config = function()
-				vim.cmd("colorscheme tokyonight")
-			end,
-		},
-		nightfox = {
-			"EdenEast/nightfox.nvim",
-			name = "nightfox",
-			config = function()
-				vim.cmd("colorscheme nightfox")
-			end,
-		},
-	}
-
-	local selected = colorschemes[scheme]
-	if selected then
-		return { selected }
-	else
-		error("Invalid colorscheme: " .. scheme)
-	end
-end
-
--- Example Usage:
-return select_colorscheme("kanagawa") -- Change to your preferred colorscheme
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	config = function()
+		vim.cmd("colorscheme catppuccin")
+	end,
+}
