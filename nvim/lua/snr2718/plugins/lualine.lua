17189b3251d176ba4f589d46ad74ffa7a287095c
snr2718 <sathya.naruto@gmail.com>
2025-09-26 10:10:42 +0000

    changed lualine theme

--- nvim/lua/snr2718/plugins/lualine.lua
+++ nvim/lua/snr2718/plugins/lualine.lua
@@ -1,23 +1,23 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  requires = {'nvim-tree/nvim-web-devicons', opt=true},
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = require("lualine.themes.base16")
-      },
-      tabline = {
-        lualine_c = {{'filename', path = 1}}
-      },
-      winbar = {
-        lualine_c = {
-            {
-                "navic",
-                color_correction = nil,
-                navic_opts = nil
-            }
-        }
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = require("lualine.themes.solarized_dark"),
+			},
+			tabline = {
+				lualine_c = { { "filename", path = 1 } },
+			},
+			winbar = {
+				lualine_c = {
+					{
+						"navic",
+						color_correction = nil,
+						navic_opts = nil,
+					},
+				},
+			},
+		})
+	end,
 }
