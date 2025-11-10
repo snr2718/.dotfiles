4025a849983459a1684f78d6900b68d6b82964d2
snr2718 <sathya.naruto@gmail.com>
2025-11-10 12:06:59 +0000

    fix "gl" not working

--- nvim/after/plugin/diagnostics.lua
+++ nvim/after/plugin/diagnostics.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
+vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 -- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
